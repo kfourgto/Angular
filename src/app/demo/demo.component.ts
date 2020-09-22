4c6608c1c4be1d3d317113ac6a9f08dfabf6692c
unknown <kfourgtofg@gmail.com>
2020-09-22 21:48:40 +0700

    Buoi0_Tao_component

---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-demo", //id, class, .... tagname(Sử dụng selector như tag name của component)
+  templateUrl: "./demo.component.html", //Nơi hiển thị nội dung ui của component(giống hàm render)
+  styleUrls: ["./demo.component.scss"], // Phần css của component này(component khác k sử dụng đc)
+})
+export class DemoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
